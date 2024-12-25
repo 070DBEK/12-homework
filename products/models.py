3445dd6c34d61edcc824627737676ffaf3ef74f8
Ozodbek <ahmadovozodbek80@gmail.com>
2024-12-25 17:34:21 +0530

    Mission completed!

---
+++ products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.shortcuts import reverse
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products_images/')
+    category = models.CharField(max_length=100)
+
+    def get_detail_url(self):
+        return reverse('products:detail', args=[self.pk])
